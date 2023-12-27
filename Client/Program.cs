2226a8f37547146a60271946467b9a7a2d6dd9be
Christoffer Jansson <christofferjansson1@gmail.com>
2023-12-27 21:55:07 +0100

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+using BlazorChat_SignalR.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorChat_SignalR.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
